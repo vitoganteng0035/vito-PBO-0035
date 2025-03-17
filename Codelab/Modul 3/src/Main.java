80fdbae65cf0682f811657e1ad1d41c59f99580b
vitoganteng0035 <allamsultan112@webmail.umm.ac.id>
2025-03-17 09:24:19 +0700

    Modul 3

---
+++ Codelab/Modul 3/src/Main.java
@@ -0,0 +1,72 @@
+// Kelas utama Main
+public class Main {
+    public static void main(String[] args) {
+        KarakterGame umum = new KarakterGame("Karakter Umum", 100);
+        Pahlawan brimstone = new Pahlawan("Brimstone", 150);
+        Musuh viper = new Musuh("Viper", 200);
+
+        // Menampilkan status awal
+        System.out.println("Status awal:");
+        System.out.println(brimstone.getNama() + " memiliki kesehatan: " + brimstone.getKesehatan());
+        System.out.println(viper.getNama() + " memiliki kesehatan: " + viper.getKesehatan());
+
+        // Simulasi serangan
+        brimstone.serang(viper);
+        viper.serang(brimstone);
+    }
+}
+// Superclass KarakterGame
+class KarakterGame {
+    private String nama;
+    private int kesehatan;
+
+    public KarakterGame(String nama, int kesehatan) {
+        this.nama = nama;
+        this.kesehatan = kesehatan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getKesehatan() {
+        return kesehatan;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public void serang(KarakterGame target) {
+        // Akan dioverride di subclass
+    }
+}
+
+// Subclass Pahlawan
+class Pahlawan extends KarakterGame {
+    public Pahlawan(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    @Override
+    public void serang(KarakterGame target) {
+        System.out.println(getNama() + " menyerang " + target.getNama() + " menggunakan Orbital Strike!");
+        target.setKesehatan(target.getKesehatan() - 20);
+        System.out.println(target.getNama() + " sekarang memiliki kesehatan " + target.getKesehatan());
+    }
+}
+
+// Subclass Musuh
+class Musuh extends KarakterGame {
+    public Musuh(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    @Override
+    public void serang(KarakterGame target) {
+        System.out.println(getNama() + " menyerang " + target.getNama() + " menggunakan Snake Bite!");
+        target.setKesehatan(target.getKesehatan() - 15);
+        System.out.println(target.getNama() + " sekarang memiliki kesehatan " + target.getKesehatan());
+    }
+}
+
